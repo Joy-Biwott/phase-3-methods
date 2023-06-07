f98b0d58ec7329c934fe26e99301613ee9ad3b21
Joy <biwott.joy.03@gmail.com>
2023-06-07 08:55:22 +0300

    Ruby-methods

--- methods.rb
+++ methods.rb
@@ -1,1 +1,27 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+puts greet_programmer
+
+def greet name="Naureen"
+    puts "Hello, #{name}!"
+end
+
+
+def greet_with_default name="programmer"
+    puts "Hello, #{name}!"
+end
+
+
+def add num1, num2
+    num1 + num2
+end
+
+def halve number
+    if number.class == Integer
+        return number /2
+    end
+    nil
+end
+
